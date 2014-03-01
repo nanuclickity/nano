3ed879b407a50d74dfb4210a8e7430e3d295204d
Tushar Kant <nanu.clickity@gmail.com>
2014-03-01 23:39:33 +0530

    Updated window dimensions

--- js/background.js
+++ js/background.js
@@ -1,10 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     "bounds": {
-      "width": 1000,
-      "height": 540,
+      "width": 285,
+      "height": 341,
       "left": (screen.width / 2) - 500,
       "top": (screen.height / 2) - 270
-    }
+    },
+    "frame": "none",
   }, function(win){ win.console.log("NANO: Ready");  });
 });
